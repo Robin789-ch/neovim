03091e5d288060bb4627625dde00a4f8887d0992
Robin789-ch <robin.ottinger@gmail.com>
2026-08-22 18:38:11 +0200

    lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,90 +1,69 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     config = function()
-      vim.lsp.enable('pyrefly')
-
+      -- Rust
       vim.lsp.enable('rust_analyzer')
 
+      -- Lua / Neovim
       vim.lsp.config('lua_ls', {
-        on_init = function(client)
-          if client.workspace_folders then
-            local path = client.workspace_folders[1].name
-            if
-                path ~= vim.fn.stdpath('config')
-                and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
-            then
-              return
-            end
-          end
-
-          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+        settings = {
+          Lua = {
             runtime = {
-              -- Tell the language server which version of Lua you're using (most
-              -- likely LuaJIT in the case of Neovim)
               version = 'LuaJIT',
-              -- Tell the language server how to find Lua modules same way as Neovim
-              -- (see `:h lua-module-load`)
-              path = {
-                'lua/?.lua',
-                'lua/?/init.lua',
-              },
             },
-            -- Make the server aware of Neovim runtime files
             workspace = {
               checkThirdParty = false,
               library = {
                 vim.env.VIMRUNTIME,
-                -- Depending on the usage, you might want to add additional paths
-                -- here.
-                -- '${3rd}/luv/library',
-                -- '${3rd}/busted/library',
               },
-              -- Or pull in all of 'runtimepath'.
-              -- NOTE: this is a lot slower and will cause issues when working on
-              -- your own configuration.
-              -- See https://github.com/neovim/nvim-lspconfig/issues/3189
-              -- library = vim.api.nvim_get_runtime_file('', true),
             },
-          })
-        end,
-        settings = {
-          Lua = {},
+          },
         },
       })
       vim.lsp.enable('lua_ls')
 
-      vim.api.nvim_create_autocmd('LspAttach', {
-        group = vim.api.nvim_create_augroup('my.lsp', {}),
-        callback = function(ev)
-          local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-          if client:supports_method('textDocument/implementation') then
-            -- Create a keymap for vim.lsp.buf.implementation ...
-          end
+      -- Format Lua with lua_ls
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.lua',
+        callback = function()
+          vim.lsp.buf.format({
+            name = 'lua_ls',
+            timeout_ms = 1000,
+          })
+        end,
+      })
 
-          -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-          if client:supports_method('textDocument/completion') then
-            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-            -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            -- client.server_capabilities.completionProvider.triggerCharacters = chars
+      -- Python
+      vim.lsp.config('ruff', {
+        init_options = {
+          settings = {
+            -- Prefer project pyproject.toml / ruff.toml over editor defaults
+            configurationPreference = 'filesystemFirst',
+          },
+        },
+      })
+      vim.lsp.enable('ty')
+      vim.lsp.enable('ruff')
 
-            -- vim.lsp.completion.enable(true, client.id, ev.buf, {autotrigger = true})
-          end
+      -- Format Python with Ruff on save
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*.py',
+        callback = function()
+          vim.lsp.buf.code_action({
+            context = {
+              only = { 'source.organizeImports' },
+              diagnostics = {},
+            },
+            apply = true,
+          })
 
-          -- Auto-format ("lint") on save.
-          -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-          if not client:supports_method('textDocument/willSaveWaitUntil')
-              and client:supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-              group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-              buffer = ev.buf,
-              callback = function()
-                vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-              end,
-            })
-          end
+          vim.lsp.buf.format({
+            name = 'ruff',
+            timeout_ms = 1000,
+          })
         end,
       })
     end,
-  }
+  },
 }
